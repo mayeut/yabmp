17bd06675b9d1c12c94d6cb79ad9ee1f1debd3cb
mayeut <mayeut@users.noreply.github.com>
2015-10-01 00:06:23 +0200

    Update doc

--- lib/yabmp/inc/yabmp_types.h
+++ lib/yabmp/inc/yabmp_types.h
@@ -31,31 +31,31 @@
 
 /* Let's use constants from limits.h to define yabmp integer types */
 #if CHAR_BIT == 8 && UCHAR_MAX == 255
-typedef unsigned char yabmp_uint8;
+typedef unsigned char yabmp_uint8;  /**< 8-bit unsigned integer. Unsigned integer type with width of exactly 8 bits. */
 #else
-#	error "yabmp requires 8 bit unsigned integer"
+#	error "yabmp requires an 8-bit unsigned integer type"
 #endif
 
 #if UINT_MAX == 65535
 typedef unsigned int yabmp_uint16;
 #elif USHRT_MAX == 65535
-typedef unsigned short yabmp_uint16;
+typedef unsigned short yabmp_uint16; /**< 16-bit unsigned integer. Unsigned integer type with width of exactly 16 bits. */
 #else
-#	error "yabmp requires 16 bit unsigned integer"
+#	error "yabmp requires a 16-bit unsigned integer type"
 #endif
 
 #if UINT_MAX == 4294967295
-typedef unsigned int yabmp_uint32;
-#	define YABMP_PRIu32 "u"
-# define YABMP_PRIX32 "X"
-#	define YABMP_UINT32_MAX UINT_MAX
+typedef unsigned int yabmp_uint32; /**< 32-bit unsigned integer. Unsigned integer type with width of exactly 32 bits. */
+#	define YABMP_PRIu32 "u" /**< \brief Format conversion specifier to output an unsigned decimal integer value of type #yabmp_uint32. */
+# define YABMP_PRIX32 "X" /**< \brief Format conversion specifier to output an unsigned uppercase hexadecimal integer value of type #yabmp_uint32. */
+#	define YABMP_UINT32_MAX UINT_MAX /**< Maximum value of an object of type #yabmp_uint32. */
 #elif ULONG_MAX == 4294967295
 typedef unsigned long int yabmp_uint32;
 #	define YABMP_PRIu32 "lu"
 # define YABMP_PRIX32 "lX"
 #	define YABMP_UINT32_MAX ULONG_MAX
 #else
-#	error "yabmp requires 32 bit unsigned integer"
+#	error "yabmp requires a 32-bit unsigned integer type"
 #endif
 
 #endif /* YABMP_TYPES_H */
