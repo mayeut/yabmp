4bfe029de5c45787c6ead9e2cf1062892fec735b
mayeut <mayeut@users.noreply.github.com>
2015-11-02 22:27:39 +0100

    Fix Travis cmake install

--- tools/travis-install.sh
+++ tools/travis-install.sh
@@ -25,15 +25,15 @@ if [ -d "${HOME}/cmake-install" ]; then
 else
 	echo "Retrieving CMake 3.3.2"
 	if [ "${TRAVIS_OS_NAME:-}" == "linux" ] || uname -s | grep -i Linux &> /dev/null; then
-		wget --no-check-certificate https://cmake.org/files/v3.3/cmake-3.3.2-Linux-x86_64.tar.gz
-		tar -xzf cmake-3.3.2-Linux-x86_64.tar.gz
-		#wget -qO - https://cmake.org/files/v3.3/cmake-3.3.2-Linux-x86_64.tar.gz | tar -xz
+		#wget --no-check-certificate https://cmake.org/files/v3.3/cmake-3.3.2-Linux-x86_64.tar.gz
+		#tar -xzf cmake-3.3.2-Linux-x86_64.tar.gz
+		wget --no-check-certificate -qO - https://cmake.org/files/v3.3/cmake-3.3.2-Linux-x86_64.tar.gz | tar -xz
 		# copy to a directory that will not changed every version
 		mv cmake-3.3.2-Linux-x86_64 ${HOME}/cmake-install
 	elif [ "${TRAVIS_OS_NAME:-}" == "osx" ] || uname -s | grep -i Darwin &> /dev/null; then
 		wget --no-check-certificate -qO - https://cmake.org/files/v3.3/cmake-3.3.2-Darwin-x86_64.tar.gz | tar -xz
 		# copy to a directory that will not changed every version
-		mv cmake-3.3.2-Darwin-universal/CMake.app/Contents ${HOME}/cmake-install
+		mv cmake-3.3.2-Darwin-x86_64/CMake.app/Contents ${HOME}/cmake-install
 	else
 		echo "OS not supported" && exit 1
 	fi
